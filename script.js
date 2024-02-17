e817f24d39d4ca5752f595e03b6dbf66f7175f82
Vaishnavi Singh Rajput <vaishnavirajput981@gmail.com>
2024-02-17 22:46:34 +0530

    updated files

--- script.js
+++ script.js
@@ -12,8 +12,33 @@ const imgSection = document.getElementById("img-section")
 
 async function searchImages(){
     keyword = inputText.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
+    
+    if(page === 1){
+        imgSection.innerHTML = "";
+    }
+
+    const results = data.results;
+    results.map((result) => {
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.appendChild(image);
+        imgSection.appendChild(imageLink)
+        
+    })
+    loadMoreBtn.style.display = "block";
+
 }
+searchBtn.addEventListener("click", (e) =>{
+    e.preventDefault;
+    page = 1;
+    searchImages();
+})
+loadMoreBtn.addEventListener("click", (e) =>{
+    page++;
+    searchImages();
+})
